27372b98d316e72274b7dc5ea55a57feb9158ab9
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-05-20 23:49:03 -0300

    Versionando o banco de dados com o Flyway

---
+++ src/main/java/com/paulo/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,11 @@
+package com.paulo.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+	
+}
---
+++ src/main/java/com/paulo/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,6 @@
+package com.paulo.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO
+}
